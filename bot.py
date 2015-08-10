57ec794995fbfff9bb78d02fc23355bf8182a58b
Karan Goel <karan@goel.io>
2015-08-09 17:53:45 -0700

    many fixes

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # Built-in imports
 import logging
 import os
+import random
 import time
 
 # Third-party dependencies
@@ -26,9 +27,10 @@ MAX_TWEET_TEXT_LENGTH = TWEET_LENGTH - IMAGE_URL_LENGTH - 1
 DOTS = '...'
 BACKOFF = 0.5 # Initial wait time before attempting to reconnect
 MAX_BACKOFF = 300 # Maximum wait time between connection attempts
+MAX_IMAGE_SIZE = 3072 * 1024 # bytes
 
 logging.basicConfig(filename='logger.log',
-                    level=logging.DEBUG,
+                    level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
@@ -47,9 +49,10 @@ backoff = BACKOFF
 
 
 def get_gif_filename(term):
-    image = [i for i in giphy.search(term, limit=1)][0]
+    images = [i for i in giphy.search(term, limit=20) if i.filesize < MAX_IMAGE_SIZE]
+    image = random.choice(images)
     filename = 'images/%s.%s' % (term.replace(' ', '_'), image.type)
-    logging.debug('get_gif_filename: %s--%s' % (term, filename))
+    logging.info('get_gif_filename: %s--%s' % (term, filename))
 
     f = open(filename, 'wb')
     f.write(requests.get(image.media_url).content)
@@ -57,9 +60,9 @@ def get_gif_filename(term):
     return filename
 
 
-def parse_tweet(tweet_text):
+def parse_tweet(tweet_from, tweet_text):
     result = parser.parse(tweet_text)
-    tagged_users = result.users
+    tagged_users = result.users + [tweet_from]
     tagged_hashtags = result.tags
     tagged_urls = result.urls
 
@@ -70,7 +73,7 @@ def parse_tweet(tweet_text):
     for url in tagged_urls:
         tweet_text = tweet_text.replace('%s' % url, '')
 
-    logging.debug('parse_tweet: %s--%s' % (tagged_users, tweet_text))
+    logging.info('parse_tweet: %s--%s' % (tagged_users, tweet_text))
     return tagged_users, tweet_text.strip()
 
 
@@ -79,7 +82,7 @@ def generate_reply_tweet(users):
     if len(reply) > MAX_TWEET_TEXT_LENGTH:
         reply = reply[:MAX_TWEET_TEXT_LENGTH - len(DOTS)] + DOTS
 
-    logging.debug('generate_reply_tweet: %s' % reply)
+    logging.info('generate_reply_tweet: %s' % reply)
     return reply
 
 
@@ -95,28 +98,28 @@ class StreamListener(tweepy.StreamListener):
         tweet_from = status.user.screen_name
 
         if tweet_from != 'slashgif':
-            logging.debug('on_status: %s--%s' % (tweet_id, tweet_text))
+            logging.info('on_status: %s--%s' % (tweet_id, tweet_text))
 
             # Parse tweet for search term
-            tagged_users, search_term = parse_tweet(tweet_text)
+            tagged_users, search_term = parse_tweet(tweet_from, tweet_text)
 
             # Search and save the image
             filename = get_gif_filename(search_term)
             # Generate and send the the reply tweet
             reply_tweet = generate_reply_tweet(tagged_users)
-            reply_status = api.update_with_media(filename,
-                reply_tweet, tweet_id)
+            reply_status = api.update_with_media(filename=filename,
+                status=reply_tweet, in_reply_to_status_id=tweet_id)
 
-            logging.debug('on_status_sent: %s %s' % (
+            logging.info('on_status_sent: %s %s' % (
                 reply_status.id_str, reply_status.text))
 
     def on_error(self, status_code):
         global backoff
-        logging.debug('on_error: %d' % status_code)
+        logging.info('on_error: %d' % status_code)
 
         if status_code == 420:
             backoff = backoff * 2
-            logging.debug('on_error: backoff %s seconds' % backoff)
+            logging.info('on_error: backoff %s seconds' % backoff)
             time.sleep(backoff)
             return True
 
@@ -126,4 +129,8 @@ if not os.path.exists('images/'):
 
 stream_listener = StreamListener()
 stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-stream.userstream(_with='user', replies='all')
+try:
+    stream.userstream(_with='user', replies='all')
+except Exception as e:
+    logging.INFO('stream_exception: %s' % e)
+    raise e
